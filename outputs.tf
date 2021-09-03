4d279065586c6769e6db59dd161b5e8022abccad
tomrutherford <tmrutherford@gmail.com>
2021-09-02 21:59:46 -0600

    WIP

--- outputs.tf
+++ outputs.tf
@@ -4,5 +4,5 @@ output "qnap_glacier_user_access_key" {
 
 output "qnap_glacier_user_secret_access_key" {
   value     = aws_iam_access_key.qnap_glacier_iam_user_access_key.secret
-  sensitive = true
+  sensitive = false
 }
